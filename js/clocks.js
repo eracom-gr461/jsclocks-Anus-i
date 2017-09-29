eecf5b5e963b3d39aa4e749542871c923f25784a
Anus-i <31766682+Anus-i@users.noreply.github.com>
2017-09-29 14:51:50 +0200

    MODIF

--- js/clocks.js
+++ js/clocks.js
@@ -1,10 +1,12 @@
 
 /*
  * Un petit script mesurant l'écoulement du temps
- * 
+ *
  * Source: https://codepen.io/Tcip/pen/BNKjeN
  * Auteur: Mikael Elmblad, tcip.se
 */
+
+
 
 // répéter la fonction metronone une fois par seconde
 
@@ -18,10 +20,10 @@ function metronome() {
   // 1: on définit l'heure, avec l'objet Date()
   // voir documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
 
-  var d = new Date();
-  
+  var d = new Date ();
+
   // 2: on affiche l'heure dans notre objet "horloge"
 
-  document.getElementById("horloge").innerHTML = d.toLocaleTimeString();
+  document.getElementById("horloge").innerHTML = d.getHours() +":"+ d.getMinutes() +":"+ d.getSeconds();
 
 }
